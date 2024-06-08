818ee4daafdf8934afd61f8dd6c33d142f377aa5
olalekan <lekanmojibola@gmail.com>
2024-06-08 10:31:39 +0100

    small change just to test the checker

--- test_get_count.py
+++ test_get_count.py
@@ -3,9 +3,14 @@
 """
 from models import storage
 from models.state import State
+from models.amenity import Amenity
+from models.user import User
 
 print("All objects: {}".format(storage.count()))
 print("State objects: {}".format(storage.count(State)))
+print("Amenity objects: {}".format(storage.count(Amenity)))
+print("User objects: {}".format(storage.count(User)))
+
 
 first_state_id = list(storage.all(State).values())[0].id
 print(first_state_id)
