7558bb7addfc9b4e3cb0455072910d842c58f5fa
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-06-09 23:25:20 +0100

    wrote an endpoint that fetch all the places associated to a city

---
+++ api/v1/views/places.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+a new view for Place objects that handles all default
+RESTFul API actions
+"""
+
+from flask import jsonify, abort, request
+from api.v1.views import app_views
+from models import storage
+from models.city import City
+from models.place import Place
+
+
+@app_views.route("/cities/<city_id>/places", methods=["GET"],
+                 strict_slashes=False)
+def all_city_places(city_id):
+    """
+    return all the places that is linked to
+    a city
+    """
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    all_places = storage.all(Place)
+    list_places = []
+    for key, value in all_places.items():
+        if value.city_id == city_id:
+            list_places.append(value.to_dict())
+
+    return jsonify(list_places)
