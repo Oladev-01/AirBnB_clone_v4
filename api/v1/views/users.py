002eca42308edb72d9c294d12fc2d6b9244c23ec
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-06-09 23:14:21 +0100

    resolve requirement issues in the user view v4

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -4,8 +4,8 @@
 view for User objects that handles all default RESTful
 """
 
+from flask import abort, jsonify, request
 from api.v1.views import app_views
-from flask import jsonify, abort, request
 from models import storage
 from models.user import User
 
